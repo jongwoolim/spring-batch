acc3add2cf3af97dbc2cd16561dfb81a75a65c3a
limjongwoo <whddn528@naver.com>
2021-11-14 18:21:04 +0900

    init commit

---
+++ src/test/java/me/jongwoo/springbatchstudy/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jongwoo.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
