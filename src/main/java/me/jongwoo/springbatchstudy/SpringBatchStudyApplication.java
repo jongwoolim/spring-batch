3f9fe45123dd371ac6b3b93962c0927f5366392b
limjongwoo <whddn528@naver.com>
2021-12-05 22:09:16 +0900

    job parameter late binding

--- src/main/java/me/jongwoo/springbatchstudy/SpringBatchStudyApplication.java
+++ src/main/java/me/jongwoo/springbatchstudy/SpringBatchStudyApplication.java
@@ -6,13 +6,17 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.scope.context.StepContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.config.Task;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -30,23 +34,35 @@ public class SpringBatchStudyApplication {
     }
 
     @Bean
-    public Step step(){
-        return this.stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello, world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-    @Bean
     public Job job(){
         return this.jobBuilderFactory.get("job")
                 .start(step())
                 .build();
     }
+
+    @Bean
+    public Step step(){
+        return this.stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet(null)).build();
+    }
+
+    @StepScope // 늦은 바인딩을 이용한 잡 파라미터 주입
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name) {
+        return (stepContribution, chunkContext) -> {
+//            String name = (String) chunkContext.getStepContext()
+//                    .getJobParameters()
+//                    .get("name");
+
+            System.out.println(String.format("hello, %s!,", name));
+
+            return RepeatStatus.FINISHED;
+        };
+
+
+    }
+
+
 
 }
