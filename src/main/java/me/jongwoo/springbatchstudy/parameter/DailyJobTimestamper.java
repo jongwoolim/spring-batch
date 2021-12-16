1f2934306f94cf241a289bac35513ed42c682d10
limjongwoo <whddn528@naver.com>
2021-12-16 21:13:12 +0900

    DailyJobTimestamper add

---
+++ src/main/java/me/jongwoo/springbatchstudy/parameter/DailyJobTimestamper.java
@@ -0,0 +1,18 @@
+package me.jongwoo.springbatchstudy.parameter;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
+
+import java.util.Date;
+
+public class DailyJobTimestamper implements JobParametersIncrementer {
+
+    @Override
+    public JobParameters getNext(JobParameters jobParameters) {
+
+        return new JobParametersBuilder(jobParameters)
+                .addDate("currentDate", new Date())
+                .toJobParameters();
+    }
+}
