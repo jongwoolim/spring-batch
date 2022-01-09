8be138034aa10215bb276d0cdc258a8ba3f77d4f
limjongwoo <whddn528@naver.com>
2022-01-09 20:29:56 +0900

    RandomDecider add

--- src/main/java/me/jongwoo/springbatchstudy/job/ConditionalJob.java
+++ src/main/java/me/jongwoo/springbatchstudy/job/ConditionalJob.java
@@ -1,10 +1,12 @@
 package me.jongwoo.springbatchstudy.job;
 
 import lombok.RequiredArgsConstructor;
+import me.jongwoo.springbatchstudy.RandomDecider;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -43,14 +45,32 @@ public class ConditionalJob {
         };
     }
 
+//    @Bean
+//    public Job conditionalTestJob(){
+//        return this.jobBuilderFactory.get("conditionalJob")
+//                .start(firstStep())
+//                .on("FAILED").to(failureStep())// 스텝의 ExitStatus를 평가해 어떤 일을 수행할 지 결정
+//                .from(firstStep()).on("*").to(successStep()) // * 0개 이상의 문자를 일치 ? 1개의 문자 일치
+//                .end()
+//                .build();
+//    }
+
     @Bean
     public Job conditionalTestJob(){
         return this.jobBuilderFactory.get("conditionalJob")
                 .start(firstStep())
-                .on("FAILED").to(failureStep())// 스텝의 ExitStatus를 평가해 어떤 일을 수행할 지 결정
-                .from(firstStep()).on("*").to(successStep()) // * 0개 이상의 문자를 일치 ? 1개의 문자 일치
+                .next(decider())
+                .from(decider())
+                .on("FAILED").to(failureStep())
+                .from(decider())
+                .on("*").to(successStep())
                 .end()
                 .build();
+    }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new RandomDecider();
     }
 
 
