28a3c309556df143cd8c655f234918d59cc771e9
limjongwoo <whddn528@naver.com>
2022-01-19 21:10:15 +0900

    Rest Job Step add

---
+++ src/main/java/me/jongwoo/springbatchstudy/job/RestJob.java
@@ -0,0 +1,37 @@
+package me.jongwoo.springbatchstudy.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestJob {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job restJob(){
+        return this.jobBuilderFactory.get("rest job")
+                .incrementer(new RunIdIncrementer())
+                .start(restStep())
+                .build();
+    }
+
+    @Bean
+    public Step restStep() {
+        return this.stepBuilderFactory.get("rest step")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step 1 ran today !!!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
