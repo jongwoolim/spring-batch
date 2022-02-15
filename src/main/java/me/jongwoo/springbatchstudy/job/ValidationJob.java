2a0d8d90b21efcf06c54c440362001711890c1d0
jongwoolim <whddn528@naver.com>
2022-02-15 15:37:52 +0900

    ClassifierCompositeItemProcessor add

--- src/main/java/me/jongwoo/springbatchstudy/job/ValidationJob.java
+++ src/main/java/me/jongwoo/springbatchstudy/job/ValidationJob.java
@@ -3,21 +3,24 @@ package me.jongwoo.springbatchstudy.job;
 import lombok.RequiredArgsConstructor;
 import me.jongwoo.springbatchstudy.domain.Customer2;
 import me.jongwoo.springbatchstudy.processor.UniqueLastNameValidator;
+import me.jongwoo.springbatchstudy.processor.ZipCodeClassifier;
 import me.jongwoo.springbatchstudy.service.UpperCaseNameService;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.adapter.ItemProcessorAdapter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.support.ClassifierCompositeItemProcessor;
 import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.batch.item.support.ScriptItemProcessor;
-import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.classify.Classifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -68,18 +71,31 @@ public class ValidationJob {
 
     }
 
-    // CompositeItemProcessor add
     @Bean
-    public CompositeItemProcessor<Customer2, Customer2> itemProcessor(){
-        CompositeItemProcessor<Customer2, Customer2> itemProcessor = new CompositeItemProcessor<>();
+    public Classifier classifier(){
+        return new ZipCodeClassifier(upperCaseItemProcessor(null), lowerCaseItemProcessor(null));
+    }
 
-        itemProcessor.setDelegates(Arrays.asList(
-                customer2ValidatingItemProcessor(),
-                upperCaseItemProcessor(null),
-                lowerCaseItemProcessor(null)));
+    @Bean
+    public ClassifierCompositeItemProcessor<Customer2, Customer2> itemProcessor(){
+        ClassifierCompositeItemProcessor<Customer2, Customer2> itemProcessor = new ClassifierCompositeItemProcessor<>();
+        itemProcessor.setClassifier(classifier());
 
         return itemProcessor;
     }
+
+//    // CompositeItemProcessor add
+//    @Bean
+//    public CompositeItemProcessor<Customer2, Customer2> itemProcessor(){
+//        CompositeItemProcessor<Customer2, Customer2> itemProcessor = new CompositeItemProcessor<>();
+//
+//        itemProcessor.setDelegates(Arrays.asList(
+//                customer2ValidatingItemProcessor(),
+//                upperCaseItemProcessor(null),
+//                lowerCaseItemProcessor(null)));
+//
+//        return itemProcessor;
+//    }
 
     @Bean
     @StepScope
