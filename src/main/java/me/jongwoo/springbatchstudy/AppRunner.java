5d185800320b75aaa24a27185434ee540b59521f
limjongwoo <whddn528@naver.com>
2022-02-17 20:19:30 +0900

    db uername,password 암호화

--- src/main/java/me/jongwoo/springbatchstudy/AppRunner.java
+++ src/main/java/me/jongwoo/springbatchstudy/AppRunner.java
@@ -1,6 +1,9 @@
 package me.jongwoo.springbatchstudy;
 
 import me.jongwoo.springbatchstudy.repository.CustomerRepository;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -19,6 +22,12 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setProvider(new BouncyCastleProvider());
+        encryptor.setPoolSize(2);
+        encryptor.setPassword("jw");
+        encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
 
 //        Resource[] resources = applicationContext.getResources("classpath:/input/customerMultiFormat*");
 //        List<String> collect = Arrays.stream(resources).map(Resource::getFilename).collect(Collectors.toList());
---
+++ src/main/java/me/jongwoo/springbatchstudy/config/JasyptConfig.java
@@ -0,0 +1,25 @@
+package me.jongwoo.springbatchstudy.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Bean("encryptBean")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setProvider(new BouncyCastleProvider());
+        encryptor.setPoolSize(2);
+        encryptor.setPassword("jw");
+        encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
+
+        return encryptor;
+    }
+}
