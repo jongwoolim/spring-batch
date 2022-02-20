54f314701b516d06007bc1ff82bd87234b9015c0
limjongwoo <whddn528@naver.com>
2022-02-20 14:42:03 +0900

    HibernateBatchConfigurer update

--- src/main/java/me/jongwoo/springbatchstudy/config/HibernateBatchConfigurer.java
+++ src/main/java/me/jongwoo/springbatchstudy/config/HibernateBatchConfigurer.java
@@ -1,11 +1,16 @@
 package me.jongwoo.springbatchstudy.config;
 
 import org.hibernate.SessionFactory;
+import org.springframework.batch.core.configuration.BatchConfigurationException;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -15,12 +20,30 @@ public class HibernateBatchConfigurer extends DefaultBatchConfigurer {
     private DataSource dataSource;
     private SessionFactory sessionFactory;
     private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
+    private JobLauncher jobLauncher;
+    private JobExplorer jobExplorer;
 
     public HibernateBatchConfigurer(DataSource dataSource, EntityManagerFactory entityManagerFactory) {
         super(dataSource);
         this.dataSource = dataSource;
         this.sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
-        this.transactionManager = new HibernateTransactionManager(this.sessionFactory);
+    }
+
+    @PostConstruct
+    public void init(){
+        try{
+            final HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager(this.sessionFactory);
+            hibernateTransactionManager.afterPropertiesSet();
+            this.transactionManager = hibernateTransactionManager;
+
+            this.jobRepository = createJobRepository();
+            this.jobExplorer = createJobExplorer();
+            this.jobLauncher = createJobLauncher();
+        }catch (Exception e){
+            throw new BatchConfigurationException(e);
+        }
+
     }
 
     @Override
---
+++ src/main/java/me/jongwoo/springbatchstudy/domain/Customer4.java
@@ -0,0 +1,23 @@
+package me.jongwoo.springbatchstudy.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer4 {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+}
