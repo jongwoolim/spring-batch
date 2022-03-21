0388cd0ad1c54c865650a7ba962b8957d6ba8810
limjongwoo <whddn528@naver.com>
2022-03-21 21:36:07 +0900

    quartz cron scheduler test

--- src/main/java/me/jongwoo/springbatchstudy/config/QuartzConfiguration.java
+++ src/main/java/me/jongwoo/springbatchstudy/config/QuartzConfiguration.java
@@ -1,23 +1,29 @@
-//package me.jongwoo.springbatchstudy.config;
-//
-//import me.jongwoo.springbatchstudy.quartz.BatchScheduledJob;
-//
-//import org.quartz.*;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class QuartzConfiguration {
-//
-//    @Bean
-//    public JobDetail quartzJobDetail(){
-//        return JobBuilder.newJob(BatchScheduledJob.class)
-//                .storeDurably()
-//                .build();
-//    }
-//
-//    @Bean
-//    public Trigger jobTrigger(){
+package me.jongwoo.springbatchstudy.config;
+
+import me.jongwoo.springbatchstudy.quartz.BatchScheduledJob;
+
+import org.quartz.*;
+import org.quartz.impl.triggers.CronTriggerImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfiguration {
+
+    @Bean
+    public JobDetail quartzJobDetail(){
+        return JobBuilder.newJob(BatchScheduledJob.class)
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger jobTrigger(){
+        return TriggerBuilder
+                .newTrigger()
+                .forJob(quartzJobDetail())
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/10 * * * * ?"))
+                .build();
 //        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
 //                .withIntervalInSeconds(5).withRepeatCount(4);
 //
@@ -25,5 +31,5 @@
 //                .forJob(quartzJobDetail())
 //                .withSchedule(scheduleBuilder)
 //                .build();
-//    }
-//}
+    }
+}
